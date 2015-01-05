448e9d7d77ca48e39f51923a5a72440c8b476957
Tushar Tyagi <tushar4r@gmail.com>
2015-01-05 02:46:25 -0800

    splat

--- src/Main.hs
+++ src/Main.hs
@@ -7,9 +7,19 @@ sumeven n (x:xs) = if even x
 --eta-reduction
 calc = sumeven 0
 
+-- using Maybe
 second :: [a] -> Maybe a
 second (x:xs) = if null xs
               then Nothing
               else Just (head xs)
+
+-- another usage of Maybe
+-- lend is a function taking two params
+-- the let block defines two new expressions
+lend amount balance = let reserve = 100
+                          newBalance = balance - amount
+                      in if balance < reserve
+                         then Nothing
+                         else Just newBalance
 
 main = print (second [1])
---
+++ src/ch03/Lending.hs
@@ -0,0 +1,1 @@
+module Lending where
